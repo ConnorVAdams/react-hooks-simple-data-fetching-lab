8508adceb9c2c0155cb8421bb67e77bd36938032
Connor Adams <connorvadams@gmail.com>
2023-10-06 09:44:42 -0600

    Submits lab.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import { useState, useEffect } from 'react'
+
+const App = () => {
+    const [randomImage, setRandomImage] = useState('')
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => {
+            setRandomImage(data.message)
+            setIsLoaded(true)
+        })
+    }, [])
+
+    if (!isLoaded) return <p>Loading...</p>
+
+  return (
+    <img src={randomImage} alt='A Random Dog' />
+  )
+}
+
+export default App
